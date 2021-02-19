ce090e3100270d190a2136670e0efd02dceb9bb9
liupeixin <fengyi.mail@gmail.com>
2021-02-19 17:16:30 +0800

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint", "plugin:prettier/recommended"],
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+  },
+  rules: {},
+}
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  quoteProps: "preserve",
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  useTabs: false,
+};
